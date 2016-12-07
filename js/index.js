77473e15307b3f14faf7fdea1430b6cfabde9c4b
Kook Maeng <ykmaeng@gmail.com>
2016-12-07 21:43:59 +0900

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var password = document.getElementById("password"),
+  confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+  if (password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+$(window, document, undefined).ready(function() {
+    var head = document.getElementById("head");
+    var email = getUrlParameter('email');
+    head.innerHTML = "New account for '" + email + "'";
+});
